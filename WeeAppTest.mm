d8e928019f0ff7d0f5a8f09a83aba52ea317635b
doremi <bsdmaillist@gmail.com>
2012-02-01 21:54:40 +0800

    讓每次開啟通知都會更新日期

--- WeeAppTest.mm
+++ WeeAppTest.mm
@@ -48,25 +48,24 @@ unsigned int countdown()
 
 - (UIView *)view
 {
-    if (_view == nil)
-    {
-        _view = [[UIView alloc] initWithFrame:CGRectMake(2, 0, 316, 101)];
-        
+    if (_view != nil) {
+        [_view release];
+    };
+
+        _view = [[UIView alloc] initWithFrame:CGRectMake(2, 0, 316, 71)];
         UIImage *bg = [[UIImage imageWithContentsOfFile:@"/System/Library/WeeAppPlugins/WeeAppTest.bundle/WeeAppBackground.png"] stretchableImageWithLeftCapWidth:5 topCapHeight:71];
         UIImageView *bgView = [[UIImageView alloc] initWithImage:bg];
         bgView.frame = CGRectMake(0, 0, 316, 71);
         [_view addSubview:bgView];
         [bgView release];
-        
+
         UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 316, 71)];
         lbl.backgroundColor = [UIColor clearColor];
         lbl.textColor = [UIColor whiteColor];
-//        lbl.text = @"林軒宇是大帥哥";
         lbl.text = [NSString stringWithFormat:@"退伍倒數 %d 天唷！😄", countdown()];
         lbl.textAlignment = UITextAlignmentCenter;
         [_view addSubview:lbl];
         [lbl release];
-    }
     
     return _view;
 }
